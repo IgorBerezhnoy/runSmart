81b1f4033f544793f13bd44f52dfb90c38ca64b5
IgorBerezhnoy <i-berezhnoy@inbox.ru>
2023-12-20 02:06:51 +0800

    finished lesson #60 finished homeWork and added adaptive

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
   $('.carousel__inner').slick({
     speed: 1200,
-    prevArrow: `<button type="button" class="slick-prev"><img src="icons/left.svg"></button>`,
-    nextArrow: `<button type="button" class="slick-next"><img src="../icons/right.svg"></button>`,
+    prevArrow: `<button type="button" class="slick-prev"><img src="../icons/left.svg" alt="left"></button>`,
+    nextArrow: `<button type="button" class="slick-next"><img src="../icons/right.svg" alt="right"></button>`,
     responsive: [
       {
         breakpoint: 992,
@@ -32,4 +32,20 @@ $(document).ready(function () {
 
   toggleSlide('.catalog-item__link');
   toggleSlide('.catalog-item__back');
+
+  //Modal
+  $('[data-modal=consultation]').on('click', function () {
+    $('.overlay, #consultation').fadeIn('slow');
+  });
+  $('.modal__close').on('click', function () {
+    $('.overlay,#consultation, #thanks,#order').fadeOut('slow');
+  });
+  $('.button_mini').on('click', function () {
+    $('.overlay, #order').fadeIn('slow');
+  });
+  $('.button_mini').each(function (i) {
+    $(this).on('click', function () {
+      $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+    });
+  });
 });
