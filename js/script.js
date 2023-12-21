a0028c876ccfb08911254940da8b891543f2a307
IgorBerezhnoy <i-berezhnoy@inbox.ru>
2023-12-21 17:54:40 +0800

    corrected img for git page

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
   $('.carousel__inner').slick({
     speed: 1200,
-    prevArrow: `<button type="button" class="slick-prev"><img src="icons/left.svg" alt="left"></button>`,
-    nextArrow: `<button type="button" class="slick-next"><img src="icons/right.svg" alt="right"></button>`,
+    prevArrow: `<button type="button" class="slick-prev"><img src="runSmart/icons/left.svg" alt="left"></button>`,
+    nextArrow: `<button type="button" class="slick-next"><img src="runSmart/icons/right.svg" alt="right"></button>`,
     responsive: [{
       breakpoint: 992, settings: {
         dots: true, arrows: false, infinite: true,
