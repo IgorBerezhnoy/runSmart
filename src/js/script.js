74f0e902c8dd94dd3f06d8376f981f99eb762a82
IgorBerezhnoy <i-berezhnoy@inbox.ru>
2023-12-21 17:20:26 +0800

    finished lesson #70-#71 and added animation and fix validate of website

--- src/js/script.js
+++ src/js/script.js
@@ -100,10 +100,11 @@ $(document).ready(function () {
       $('.pageUp').fadeOut();
     }
   });
-  $('a[href^=\'#\']').click(function () {
-    const _href = $(this).attr('href');
-    $('html, body').animate({scrollTop: $(_href).offset().top + 'px'});
+  $("a[href=#up]").click(function(){
+    const _href = $(this).attr("href");
+    $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
     return false;
   });
+
   new WOW().init();
 });
